2ab4d234caeea3f353feb8328370fcac62bd255c
Precious Olusola <preciousolusola16@gmail.com>
2022-06-11 23:43:10 +0100

    initialized app, added the header section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {SectionWrapper} from './components';//Download,Features,
+import assets from './assets';
 
-function App() {
+const App = ()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SectionWrapper
+        
+        title="Your Own Store of Nifty NFTs.
+        Start Selling & Growing"
+        
+        description="Buy, store, collect NFTs, exchange & earn crypto. 
+        Join 25+ million people using ProNef Marketplace."
+
+        showBtn
+        mockupImg={assets.homeHero}
+        banner={"banner"}
+      />
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Features from "./features";
+import Download from "./download";
+import SectionWrapper from "./sectionWrapper";
+
+
+export {Features, Download, SectionWrapper};
